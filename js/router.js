7b7bb09f38d205f1e683a00d91487a9a27d224ec
Davonium Technologies <dawudaminu4321@gmail.com>
2026-08-08 12:07:59 +0100

    Create router.js

---
+++ js/router.js
@@ -0,0 +1,12 @@
+/*
+ * Marvel Chat V2
+ *
+ * Application router.
+ *
+ * Routing controls navigation.
+ * Feature modules control feature behavior.
+ */
+
+export function navigate(page) {
+    console.log(`Navigating to: ${page}`);
+}
