991c2c07fee0aa2ab9d3bac9ca158272fbc912ac
Davonium Technologies <dawudaminu4321@gmail.com>
2026-08-08 12:06:12 +0100

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,18 @@
+/*
+ * Marvel Chat V2
+ *
+ * Application controller.
+ *
+ * This module will eventually coordinate:
+ * - application startup
+ * - Firebase initialization
+ * - state initialization
+ * - router initialization
+ * - feature initialization
+ *
+ * Business logic must remain in the appropriate modules.
+ */
+
+export function initializeApp() {
+    console.log("Marvel Chat V2 application starting...");
+}
