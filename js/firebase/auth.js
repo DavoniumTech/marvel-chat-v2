ec8b655c82a841eb3f86248dece4ec0ce1fb760c
Davonium Technologies <dawudaminu4321@gmail.com>
2026-08-10 01:40:36 +0100

    Implement Firebase Authentication functions
    Added functions for user registration, login, logout, and profile updates using Firebase Authentication.

--- js/firebase/auth.js
+++ js/firebase/auth.js
@@ -6,3 +6,36 @@
  * Firebase Authentication operations
  * will be migrated here.
  */
+
+
+
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  updateProfile
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { auth } from "./config.js";
+
+export async function registerUser(email, password) {
+  return await createUserWithEmailAndPassword(auth, email, password);
+}
+
+export async function loginUser(email, password) {
+  return await signInWithEmailAndPassword(auth, email, password);
+}
+
+export async function logoutUser() {
+  return await signOut(auth);
+}
+
+export function subscribeToAuth(callback) {
+  return onAuthStateChanged(auth, callback);
+}
+
+export async function updateUserProfile(user, profileData) {
+  return await updateProfile(user, profileData);
+}
+
+export { auth };
