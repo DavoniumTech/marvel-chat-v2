27a6c8907bdde5a596ada116326fb8686d0ee710
Davonium Technologies <dawudaminu4321@gmail.com>
2026-08-10 01:39:58 +0100

    Initialize Firebase app, auth, and Firestore

--- js/firebase/config.js
+++ js/firebase/config.js
@@ -7,3 +7,16 @@
  * Production Firebase configuration will be
  * migrated during the Firebase extraction phase.
  */
+
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { firebaseConfig } from "../firebaseConfig.js";
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { app, auth, db };
+
