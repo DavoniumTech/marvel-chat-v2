f4fc972f7b3aea2cc9e62e2ea9f2755b22633ea7
Davonium Technologies <dawudaminu4321@gmail.com>
2026-08-10 01:41:18 +0100

    Import and export Firestore functions
    Added Firestore functions and database configuration export.

--- js/firebase/firestore.js
+++ js/firebase/firestore.js
@@ -6,3 +6,46 @@
  * Firestore reads, writes and reusable
  * database operations will be migrated here.
  */
+
+
+
+
+import {
+  collection,
+  doc,
+  getDoc,
+  setDoc,
+  updateDoc,
+  addDoc,
+  deleteDoc,
+  query,
+  where,
+  orderBy,
+  limit,
+  getDocs,
+  serverTimestamp,
+  increment,
+  arrayUnion,
+  arrayRemove
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { db } from "./config.js";
+
+export {
+  db,
+  collection,
+  doc,
+  getDoc,
+  setDoc,
+  updateDoc,
+  addDoc,
+  deleteDoc,
+  query,
+  where,
+  orderBy,
+  limit,
+  getDocs,
+  serverTimestamp,
+  increment,
+  arrayUnion,
+  arrayRemove
+};
